c2e9f5c71af4088e5f41fc20770267e7374bbc98
kennethhagist <kenhagist@gmail.com>
2017-12-03 19:06:24 -0800

    myFirstAngularApp added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; // <-- Import HttpModule
+import { MyComponentComponent } from './my-component/my-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // <-- Include module in our AppModules
+		HttpModule // <-- Include module in our AppModules
   ],
   providers: [],
   bootstrap: [AppComponent]
